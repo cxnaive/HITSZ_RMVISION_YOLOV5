670bab54a8fd4bdbce86b498a3837625344a9183
cxnaive <1012033254@qq.com>
2021-04-06 20:52:29 +0800

    update warm up

--- resources/start.sh
+++ resources/start.sh
@@ -1,7 +1,7 @@
 pass_word="Chen3xun27"
 home_dir="andychen"
 cd /home/${home_dir}/HITSZ_RMVISION_YOLOV5/build
-echo ${pass_word}|sudo -S nohup ./start_loop "./rmcv" >temp.txt 2>&1 &
+echo ${pass_word}|sudo -S nohup ./start_loop "./rmcv" >warm_up.txt 2>&1 &
 sleep 45s
 echo ${pass_word}|sudo -S killall start_loop
 echo ${pass_word}|sudo -S killall rmcv 
