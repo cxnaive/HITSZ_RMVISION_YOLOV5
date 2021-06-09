f0b29676984aaf130f3486336de9c205579c1e82
cxnaive <1012033254@qq.com>
2021-06-10 02:26:30 +0800

    update config

--- config/src/rmconfig.cpp
+++ config/src/rmconfig.cpp
@@ -74,6 +74,7 @@ void RmConfig::init_from_file() {
     ENERGY_CAMERA_GAIN = data["ENERGY_CAMERA_GAIN"].asInt();
     ENERGY_CAMERA_EXPOSURE = data["ENERGY_CAMERA_EXPOSURE"].asInt();
     ENEMY_COLOR = data["ENEMY_COLOR"].asInt();
+    ENERGY_DELAY_TIME = data["ENERGY_DELAY_TIME"].asDouble();
     ANTI_TOP = data["ANTI_TOP"].asInt();
     MANUAL_DELTA_X = data["MANUAL_DELTA_X"].asDouble();
     MANUAL_DELTA_Y = data["MANUAL_DELTA_Y"].asDouble();
@@ -179,6 +180,7 @@ void RmConfig::write_to_file() {
     data["ENERGY_CAMERA_GAIN"] = ENERGY_CAMERA_GAIN;
     data["ENERGY_CAMERA_EXPOSURE"] = ENERGY_CAMERA_EXPOSURE;
     data["ENEMY_COLOR"] = ENEMY_COLOR;
+    data["ENERGY_DELAY_TIME"] = ENERGY_DELAY_TIME;
     data["ANTI_TOP"] = ANTI_TOP;
     data["MANUAL_DELTA_X"] = MANUAL_DELTA_X;
     data["MANUAL_DELTA_Y"] = MANUAL_DELTA_Y;
