bbbd9d4123ab31816f6307bc9b82577b54d32351
cxnaive <1012033254@qq.com>
2021-07-19 22:20:39 +0800

    update tracking

--- armor/src/armor_finder/tracking_state/tracking_state.cpp
+++ armor/src/armor_finder/tracking_state/tracking_state.cpp
@@ -7,17 +7,26 @@
 
 bool ArmorFinder::stateTrackingTarget(cv::Mat &src) {
     auto res = filterArmorInfoByColor(netDetector.detect(src),src);
-    ArmorInfo target;
+    ArmorInfo target,lost_possible_target;
     double dist_min = last_box.rect.height * 2.0;
+    double lost_dist_min = INT_MAX;
     for (auto it : res) {
-        if (it.id == last_box.id) {
-            double dist_now =
+        double dist_now =
                 getPointLength(last_box.getCenter() - it.getCenter());
+        if (it.id == last_box.id) {
             if(dist_now < dist_min){
                 dist_min = dist_now;
                 target = it;
             }
         }
+        if(dist_now < lost_dist_min){
+            lost_possible_target = it;
+            lost_dist_min = dist_now;
+        }
+    }
+    //猜测目标（神经网络id判断失误
+    if(target.bbox == cv::Rect() && lost_possible_target.bbox != cv::Rect() && lost_dist_min < last_box.rect.height / 1.5){
+        target = lost_possible_target;
     }
     //脱离追踪
     if(target.bbox == cv::Rect()){
