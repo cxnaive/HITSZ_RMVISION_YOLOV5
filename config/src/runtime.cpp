a6f804164cb2ff2a17655e8279eac4944b6ac993
cxnaive <1012033254@qq.com>
2021-07-30 06:01:46 +0800

    update video

--- config/src/runtime.cpp
+++ config/src/runtime.cpp
@@ -5,21 +5,21 @@
 double getPointLength(const cv::Point2f &p) {
     return sqrt(p.x * p.x + p.y * p.y);
 }
-cudaVideoWriter initVideoWriter(const std::string &filename_prefix) {
+cv::VideoWriter initVideoWriter(const std::string &filename_prefix) {
     std::string file_name = filename_prefix + ".avi";
-    cudaVideoWriter video = cv::cudacodec::createVideoWriter(file_name,cv::Size(640,640),60);
+    cv::VideoWriter video(file_name,cv::VideoWriter::fourcc('P','I','M','1'),60,cv::Size(640,640));
     return video;
 }
-std::map<std::string,cudaVideoWriter> video_writers;
-cudaVideoWriter getVideoWriter(std::string prefix){
+std::map<std::string,cv::VideoWriter> video_writers;
+cv::VideoWriter getVideoWriter(std::string prefix){
     auto iter = video_writers.find(prefix);
     if(iter != video_writers.end()) return iter->second;
-    cudaVideoWriter now = initVideoWriter("video/"+prefix);
+    cv::VideoWriter now = initVideoWriter("video/"+prefix);
     video_writers[prefix] = now;
     return now;
 }
 void saveVideos(cv::Mat& img,std::string prefix){
     if(img.empty()) return;
-    cudaVideoWriter writer = getVideoWriter(prefix);
-    writer->write(img);
+    cv::VideoWriter writer = getVideoWriter(prefix);
+    writer.write(img);
 }
